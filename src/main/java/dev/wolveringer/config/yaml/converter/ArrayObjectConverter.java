d7a8fb70e8fa875448868c659601066ab919dab3
Markus <markus.hadenfeldt@web.de>
2017-09-23 21:50:40 +0200

    Some updates

--- src/main/java/dev/wolveringer/config/yaml/converter/ArrayObjectConverter.java
+++ src/main/java/dev/wolveringer/config/yaml/converter/ArrayObjectConverter.java
@@ -6,10 +6,8 @@ import dev.wolveringer.config.exception.InternalConfigException;
 import lombok.NonNull;
 
 import java.lang.reflect.Array;
-import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,12 +22,11 @@ public class ArrayObjectConverter extends ValueConverter {
 
     @Override
     public Object convertFromConfig(ConfigConfiguration cfg, Class<?> targetClass, Type type, @NonNull Object input) throws Exception {
-        if(!targetClass.isArray())
-            throw new InternalConfigException("Target class isnt an array");
+        if(!targetClass.isArray()) throw new InternalConfigException("Target class isnt an array");
 
         System.out.println("Convert to " + targetClass);
 
-        Class<?> typeClass = targetClass.getComponentType(); //Must be a class. Else invalid list!
+        Class<?> typeClass = targetClass.getComponentType();
         List elements = (List) input;
         Object array = Array.newInstance(typeClass, elements.size());
 
@@ -51,10 +48,9 @@ public class ArrayObjectConverter extends ValueConverter {
 
     @Override
     public Object convertFromInstance(ConfigConfiguration cfg, ConfigFieldDescriptor field, @NonNull Object input) throws Exception {
-        if(field == null)
-            throw new InternalConfigException("Only one layer of arrays supported!");
+        if(field == null) throw new InternalConfigException("Only one layer of arrays supported!");
 
-        Class<?> typeClass = field.getField().getType().getComponentType(); //Must be a class. Else invalid list!
+        Class<?> typeClass = field.getField().getType().getComponentType();
         int length = Array.getLength(input);
 
         List output = new ArrayList<>();
